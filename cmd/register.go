7152d499ec7e54e44b9cfa65ef64c6d76d7c74ab
Mostafa Negim <mostafa.negim@gmail.com>
2024-01-15 19:56:11 +0330

    Add: register command

---
+++ cmd/register.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// registerCmd represents the register command
+var registerCmd = &cobra.Command{
+	Use:   "register",
+	Short: "",
+	Long:  `gitlab-runner register - register a new runner`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("register called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(registerCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// registerCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
